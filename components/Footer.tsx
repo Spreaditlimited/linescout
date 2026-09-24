7291273bbfb169f879fd08aa12adf15b499b1072
Tochukwu Nkwocha <nkwochatochukwu@gmail.com>
2026-09-24 13:02:21 +0100

    Serve footer logo locally

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,5 +2,5 @@ import BrandFooter from '@/components/BrandFooter';
 import FooterNewsletterForm from '@/components/FooterNewsletterForm';
 
 export default function Footer() {
-  return <BrandFooter home="https://www.sureimports.com" mainSite="https://www.sureimports.com" logo="https://www.sureimports.com/images/svg-logo-white.svg" newsletter={<FooterNewsletterForm />} />;
+  return <BrandFooter home="https://www.sureimports.com" mainSite="https://www.sureimports.com" logo="/images/svg-logo-white.svg" newsletter={<FooterNewsletterForm />} />;
 }
